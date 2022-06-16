563ffdfc465efab35bc33f7e1a029be86f7ce1af
dgkcle\vvalerio <veronica.valerio@gmail.com>
2022-06-16 01:08:22 -0300

    desafio11

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -4,35 +4,28 @@ import { useParams } from "react-router";
 import ItemDetail from "./ItemDetail";
 import { firestoreFetchOne } from "../utils/firestoreFetch";
 
-
-
 const ItemDetailContainer = () => {
 
-const [dato, setDato] = useState([]);
+    const [dato, setDato] = useState([]);
 
-const { id } = useParams();
+    const { id } = useParams();
 
-useEffect(() => {
+    useEffect(() => {
 
-firestoreFetchOne(id)
+    firestoreFetchOne(id)
 
-.then(result => setDato(result))
+    .then(result => setDato(result))
 
-.catch(err => console.log(err));
+    .catch(err => console.log(err));
 
-}, [id]);
+    }, [id]);
 
 console.log(dato)
 
-return (
-
-<> <ItemDetail item={dato} />
-
-</>
-
-);
-
+    
+    return (
+        <ItemDetail item={dato} />
+    );
 }
 
 export default ItemDetailContainer;
-
